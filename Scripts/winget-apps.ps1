cda4ad866f8f26cb7308c0c6ab86eacd4639743e
Matthew Elgert <mdelgert@yahoo.com>
2024-09-15 23:41:39 -0400

    Add example scripts

---
+++ Scripts/winget-apps.ps1
@@ -0,0 +1,13 @@
+# winget-apps.ps1
+
+# Install PowerShell
+winget install --id=Microsoft.PowerShell --silent --accept-source-agreements --accept-package-agreements
+
+# Install Brave Browser
+winget install --id=Brave.Brave --silent --accept-source-agreements --accept-package-agreements
+
+# Install Visual Studio Code
+winget install --id=Microsoft.VisualStudioCode --silent --accept-source-agreements --accept-package-agreements
+
+# Install Git
+winget install --id=Git.Git --silent --accept-source-agreements --accept-package-agreements
